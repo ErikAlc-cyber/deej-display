f617cef31299d8fb3c00f45c054bfb3dc417d8ac
ERIK COVARRUBIAS ALCANTARA <61327379+ErikAlc-cyber@users.noreply.github.com>
2024-03-23 02:51:36 +0000

    First Change

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,7 +1,19 @@
-const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSans9pt7b.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const int NUM_SLIDERS = 4;
+const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3};
 
 int analogSliderValues[NUM_SLIDERS];
+String analogSliderNames[NUM_SLIDERS] = {"Master","Mic","Music","Other"};
 
 void setup() { 
   for (int i = 0; i < NUM_SLIDERS; i++) {
@@ -9,6 +21,12 @@ void setup() {
   }
 
   Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000); 
 }
 
 void loop() {
@@ -25,17 +43,36 @@ void updateSliderValues() {
 }
 
 void sendSliderValues() {
-  String builtString = String("");
-
+  String builtString = String(""); 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.setFont(&FreeSans9pt7b);
+  display.println("Vol Controller"); // Title of screen (?)
+  display.setFont();
+  
   for (int i = 0; i < NUM_SLIDERS; i++) {
     builtString += String((int)analogSliderValues[i]);
-
+    int aux_display = i+2;
     if (i < NUM_SLIDERS - 1) {
       builtString += String("|");
     }
+    display.setCursor(0, (10*aux_display));
+    display.println(analogSliderNames[i]);
+    //display.setCursor(60, (10*i));
+    display.drawRect(60, (10*aux_display), 50, 8, WHITE);
+    int percentage = percentage_volume(analogSliderValues[i]);
+    display.fillRect(60, (10*aux_display), percentage, 8, WHITE);
+    //display.println(String((int)analogSliderValues[i]));
   }
-  
+
+  display.display();
   Serial.println(builtString);
+}
+
+int percentage_volume(int actual_value){
+  return (actual_value * 50) / 1023;
 }
 
 void printSliderValues() {
